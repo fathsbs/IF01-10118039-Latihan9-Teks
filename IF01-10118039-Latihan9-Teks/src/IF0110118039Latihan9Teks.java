7ec77b81ae94418f45301a87cf4ec6189610aa79
fathsbs <55565303+fathsbs@users.noreply.github.com>
2019-09-23 20:08:37 +0700

    Add files via upload

---
+++ IF01-10118039-Latihan9-Teks/src/IF0110118039Latihan9Teks.java
@@ -0,0 +1,21 @@
+/*
+ * NAMA              : FATAHILLAH SATRIA BIMA SENO
+ * KELAS             : IF-01
+ * NIM               : 10118039
+ * DESKRIPSI PROGRAM :   
+ */
+
+public class IF0110118039Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char:" + tab + " berisi " + c);
+    }
+
+}
